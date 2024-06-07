fb35c36536f0e8e42afa1ff1c47c8ffde065c5c6
Irvingjx <98728438+Irvingjx@users.noreply.github.com>
2024-06-06 20:53:55 -0600

    Add files via upload

---
+++ Script5.js
@@ -0,0 +1,302 @@
+const aulas02 = document.getElementById('Labs1');
+
+const svgAulas1 = document.getElementById('Capa_Lab1')
+
+let aulas = ['LC100', 'LC102', 'LC103', 'LC112', 'LC113', 'LC114', 'LC121', 'LC122', 'LC117', 'LC118', 'LC119', 'LC128', 'LC132'];
+
+let isDragging = false;
+let startPositionX = 0;
+let startPositionY = 0;
+
+// Variables para aulas02
+let scale = 1;
+let translateX = 0;
+let translateY = 0;
+
+let dataM;
+let dataP;
+
+function redirect() {
+    window.location.href = 'index.html'; // Reemplaza con la URL a la que quieres redirigir
+}
+
+document.getElementById('resetZoomButton').addEventListener('click', resetZoom);
+
+function resetZoom() {
+    // Restablecer escala y transformación a los valores iniciales
+    scale = 1;
+    translateX = 0;
+    translateY = 0;
+    aulas02.style.transform = `translate(${translateX}px, ${translateY}px) scale(${scale})`;
+}
+
+
+aulas02.addEventListener('mousedown', (event) => {
+    isDragging = true;
+    startPositionX = event.clientX - translateX;
+    startPositionY = event.clientY - translateY;
+    aulas02.classList.add('dragging');
+});
+
+aulas02.addEventListener('mouseup', () => {
+    isDragging = false;
+    aulas02.classList.remove('dragging');
+});
+
+aulas02.addEventListener('mousemove', (event) => {
+    if (isDragging) {
+        const currentX = event.clientX - startPositionX;
+        const currentY = event.clientY - startPositionY;
+        translateX = currentX;
+        translateY = currentY;
+        aulas02.style.transform = `translate(${currentX}px, ${currentY}px) scale(${scale})`;
+
+    }
+});
+
+// Evento de rueda para aulas02
+aulas02.addEventListener('wheel', (event) => {
+    // Escala y transformación aquí (sin event.preventDefault())
+    scale += event.deltaY * -0.001;
+    scale = Math.min(Math.max(0.8, scale), 8);
+    aulas02.style.transform = `translate(${translateX}px, ${translateY}px) scale(${scale})`;
+});
+
+const textoSVG = document.createElementNS("http://www.w3.org/2000/svg", "text");
+textoSVG.setAttribute("x", 910);
+textoSVG.setAttribute("y", 63); // Ajusta la posición vertical según tus necesidades
+textoSVG.setAttribute("text-anchor", "middle");  
+textoSVG.setAttribute('class', 'nombreEd'); 
+textoSVG.textContent = "EDIFICIO CENTRAL SUR";
+svgAulas1.appendChild(textoSVG);
+
+const textoSVG2 = document.createElementNS("http://www.w3.org/2000/svg", "text");
+textoSVG2.setAttribute("x", 910);
+textoSVG2.setAttribute("y", 465); // Ajusta la posición vertical según tus necesidades
+textoSVG2.setAttribute("text-anchor", "middle");  
+textoSVG2.setAttribute('class', 'nombreEd'); 
+textoSVG2.textContent = "EDIFICIO CENTRAL NORTE";
+svgAulas1.appendChild(textoSVG2);
+
+
+function agregarDes(pos,x,y,texto,textoM,textoP){
+    const textoSVG = document.createElementNS("http://www.w3.org/2000/svg", "text");
+    // const textoSVGM = document.createElementNS("http://www.w3.org/2000/svg", "text");
+    const fondoBlanco = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+
+    fondoBlanco.setAttribute("x", x-22+8);
+    fondoBlanco.setAttribute("y", y-41+2);
+    fondoBlanco.setAttribute("width", "3%");
+    fondoBlanco.setAttribute("height", "2.4%");
+    fondoBlanco.setAttribute("rx", 4)
+    fondoBlanco.setAttribute("fill", "#C4E6EF"); // Color de fondo blanco
+    fondoBlanco.setAttribute("stroke", "black"); // Color del borde
+    fondoBlanco.setAttribute("stroke-width", "1");
+    svgAulas1.appendChild(fondoBlanco);
+
+    textoSVG.setAttribute("x", x-11+25);
+    textoSVG.setAttribute("y", y-27+2);
+    //textoSVG.setAttribute("font-family", "Arial");
+    textoSVG.setAttribute("text-anchor", "middle"); 
+    textoSVG.setAttribute("font-size", "14");
+    textoSVG.setAttribute("fill", "black");
+    textoSVG.setAttribute("font-weight", "bold");
+    textoSVG.textContent = texto; 
+    svgAulas1.appendChild(textoSVG);
+
+
+    
+      // Dividir textoM en dos partes basadas en la longitud máxima permitida para la primera línea
+    const maxLengthFirstLine = 24; //Nombre de materia
+    let primeraParte = '';
+    let segundaParte = '';
+
+    if (textoM.length > maxLengthFirstLine) {
+        let words = textoM.split(' ');
+        let currentLineLength = 0;
+
+        for (let i = 0; i < words.length; i++) {
+            let word = words[i];
+            let wordLength = word.length;
+
+            if (currentLineLength + wordLength <= maxLengthFirstLine) {
+                primeraParte += word + ' ';
+                currentLineLength += wordLength + 1;
+            } else {
+                segundaParte = words.slice(i).join(' ');
+                break;
+            }
+        }
+
+        // Eliminar el espacio en blanco adicional al final de la primera parte
+        primeraParte = primeraParte.trim();
+    } else {
+        primeraParte = textoM;
+    }
+
+
+    // console.log(primeraParte);
+    // console.log(segundaParte);
+
+     // Crear y configurar el primer elemento <text> para la primera parte del texto
+    const textoSVGM1 = document.createElementNS("http://www.w3.org/2000/svg", "text");
+    textoSVGM1.setAttribute("x", x +15);
+    textoSVGM1.setAttribute("y", y + 35-2); // Ajusta la posición vertical según tus necesidades
+    textoSVGM1.setAttribute("text-anchor", "middle");  
+    textoSVGM1.setAttribute('class', 'resaltado'); 
+    textoSVGM1.textContent = primeraParte;
+    svgAulas1.appendChild(textoSVGM1);
+
+    // Crear y configurar el segundo elemento <text> para la segunda parte del texto si existe
+    if (segundaParte !== '') {
+        const textoSVGM2 = document.createElementNS("http://www.w3.org/2000/svg", "text");
+        textoSVGM2.setAttribute("x", x + 15);
+        textoSVGM2.setAttribute("y", y + 48-2); // Ajusta la posición vertical según tus necesidades
+        textoSVGM2.setAttribute("text-anchor", "middle");    
+        textoSVGM2.setAttribute('class', 'resaltado'); 
+        textoSVGM2.textContent = segundaParte;
+        svgAulas1.appendChild(textoSVGM2);
+    }
+
+    //Dividir nombre de profe.
+    // Dividir textoM en dos partes basadas en la longitud máxima permitida para la primera línea
+    const maxLengthFirstLine2 = 14; //Nombre de profe
+    let primeraParte2 = '';
+    let segundaParte2 = '';
+
+    if (textoP.length > maxLengthFirstLine2) {
+        let words = textoP.split(' ');
+        let currentLineLength = 0;
+
+        for (let i = 0; i < words.length; i++) {
+            let word = words[i];
+            let wordLength = word.length;
+
+            if (currentLineLength + wordLength <= maxLengthFirstLine2) {
+                primeraParte2 += word + ' ';
+                currentLineLength += wordLength + 1;
+            } else {
+                segundaParte2 = words.slice(i).join(' ');
+                break;
+            }
+        }
+
+        // Eliminar el espacio en blanco adicional al final de la primera parte
+        primeraParte2 = primeraParte2.trim();
+    } else {
+        primeraParte2 = textoP;
+    }
+
+
+    // console.log(primeraParte2);
+    // console.log(segundaParte2);
+
+     // Crear y configurar el primer elemento <text> para la primera parte del texto
+    const textoSVGM3 = document.createElementNS("http://www.w3.org/2000/svg", "text");
+    textoSVGM3.setAttribute("x", x +15);
+    textoSVGM3.setAttribute("y", y + 35-44); // Ajusta la posición vertical según tus necesidades
+    textoSVGM3.setAttribute("text-anchor", "middle");  
+    textoSVGM3.setAttribute('class', 'resaltado'); 
+    textoSVGM3.textContent = primeraParte2;
+    svgAulas1.appendChild(textoSVGM3);
+
+    // Crear y configurar el segundo elemento <text> para la segunda parte del texto si existe
+    if (segundaParte2 !== '') {
+        const textoSVGM4 = document.createElementNS("http://www.w3.org/2000/svg", "text");
+        textoSVGM4.setAttribute("x", x + 15);
+        textoSVGM4.setAttribute("y", y + 48-44); // Ajusta la posición vertical según tus necesidades
+        textoSVGM4.setAttribute("text-anchor", "middle");      
+        textoSVGM4.setAttribute('class', 'resaltado');    
+        textoSVGM4.textContent = segundaParte2;
+        svgAulas1.appendChild(textoSVGM4);
+    }
+    
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    const fecha = new Date();
+    let diaSemana = fecha.getDay();
+    let hora = fecha.getHours();
+    let minutos = fecha.getMinutes();
+    let segundos = fecha.getSeconds();
+    let horaVi = 9;
+    // diaSemana = 2;
+    // hora = 10;
+
+    // console.log(`DHM`, diaSemana, hora, minutos);  
+
+    let horaC = hora + minutos / 60 + segundos / 3600;
+
+    // Cargar todos los datos JSON
+    Promise.all([
+        fetch('aulasM.json').then(response => response.json()),
+        fetch('aulasP.json').then(response => response.json()),
+        fetch('aulasG.json').then(response => response.json())
+    ])
+    .then(([dataM, dataP, dataG]) => {
+        // console.log(dataM);
+        // console.log(dataP);
+        // console.log(dataG);
+
+        
+        if(horaC >= 7 && horaC < 8.5) {
+            horaVi = 0;
+        } else if(horaC >= 8.5 && horaC < 10) {
+            horaVi = 1;
+        } else if(horaC >= 10 && horaC < 11.5) {
+            horaVi = 2;
+        } else if(horaC >= 11.5 && horaC < 13) {
+            horaVi = 3;
+        } else if(horaC >= 13 && horaC < 14.5) {
+            horaVi = 4;
+        } else if(horaC >= 14.5 && horaC < 16) {
+            horaVi = 5;
+        } else if(horaC >= 16 && horaC < 17.5) {
+            horaVi = 6;
+        } else if(horaC >= 17.5 && horaC < 19) {
+            horaVi = 7;
+        } else if(horaC >= 19 && horaC < 20.5) {
+            horaVi = 8;
+        }
+
+        if(diaSemana === 0 || diaSemana === 6 || horaC < 7 || horaC > 22) {
+            for (let i = 0; i < 13; i++) {
+                // console.log(aulas[i]);
+                let elementoHtml = document.getElementById(aulas[i]);
+                elementoHtml.style.fill = 'gray';
+            }
+        } else {
+            for (let i = 0; i < 13; i++) {
+                let elemento = dataG[i + 45][horaVi][diaSemana - 1];
+                let elementoM = dataM[i + 45][horaVi][diaSemana - 1];
+                let elementoP = dataP[i + 45][horaVi][diaSemana - 1];
+
+                // console.log(`Min`, horaC); 
+                // console.log(aulas[i]);
+                // console.log(`dia`, diaSemana);
+                let elementoHtml = document.getElementById(aulas[i]);
+                const bbox = elementoHtml.getBBox();
+                const x = bbox.x + bbox.width / 2 - 15;
+                const y = bbox.y - 5; 
+
+                if (elementoHtml) {
+                    if (elemento === 'None' || typeof elemento === 'undefined') {
+                        elementoHtml.style.fill = 'green';
+                        // console.log(`La posición [${i + 11}]${horaVi}${diaSemana - 1} es null o undefined. Cambiando color.`);
+                    } else {
+                        elementoHtml.style.fill = 'red';
+                        // console.log(`La posición [${i + 11}]${horaVi}${diaSemana - 1} tiene el valor:`, elemento);
+                        agregarDes(bbox, x, y, elemento, elementoM, elementoP);
+                    }
+                } else {
+                    console.log(`No se encontró el elemento con la ID ${aulas[i]}.`);
+                }
+            }
+        }
+    })
+    .catch(error => {
+        console.error('Error al cargar los archivos JSON:', error);
+    });
+});
+
+
---
+++ script.js
@@ -0,0 +1,176 @@
+const svg = document.getElementById('CapaBusca')
+const aulas01 = document.getElementById('Aulas1');
+const aulas02 = document.getElementById('Aulas2');
+const aulas03 = document.getElementById('Aulas3');
+const aulas04 = document.getElementById('Aulas4');
+const Laboratorios = document.getElementById('Laboratorios');
+const Epesados = document.getElementById('Epesados');
+
+let isDragging = false;
+let startPositionX = 0;
+let startPositionY = 0;
+
+
+let scale = 1;
+let translateX = 0;
+let translateY = 0;
+
+const mapa = document.getElementById('mapa');
+
+
+mapa.addEventListener('mousedown', (event) => {
+    isDragging = true;
+    startPositionX = event.clientX - translateX;
+    startPositionY = event.clientY - translateY;
+    mapa.classList.add('dragging');
+});
+
+mapa.addEventListener('mouseup', () => {
+    isDragging = false;
+    mapa.classList.remove('dragging');
+});
+
+mapa.addEventListener('mousemove', (event) => {
+    if (isDragging) {
+        const currentX = event.clientX - startPositionX;
+        const currentY = event.clientY - startPositionY;
+        translateX = currentX;
+        translateY = currentY;
+        mapa.style.transform = `translate(${currentX}px, ${currentY}px) scale(${scale})`;
+    }
+});
+
+mapa.addEventListener('wheel', (event) => {
+    event.preventDefault();
+    scale += event.deltaY * -0.001;
+    scale = Math.min(Math.max(0.7, scale), 3); // Limitar el zoom entre 0.5 y 3
+    mapa.style.transform = `translate(${translateX}px, ${translateY}px) scale(${scale})`;
+});
+
+
+
+// Función para cargar dinámicamente el contenido de otra página
+function cargarNuevaPagina() {
+    window.location.href = 'pagAulas1.html';
+}
+
+// Evento click para cambiar la página
+document.getElementById('Aulas1').addEventListener('click', cargarNuevaPagina);
+
+document.getElementById('Aulas2').addEventListener('click', function() {
+    window.location.href = 'pagAulas2.html';
+});
+
+document.getElementById('Aulas3').addEventListener('click', function() {
+    window.location.href = 'pagAulas3.html';
+});
+
+document.getElementById('Aulas4').addEventListener('click', function() {
+    window.location.href = 'pagAulas4.html';
+});
+
+document.getElementById('Laboratorios').addEventListener('click', function() {
+    window.location.href = 'pagLabs.html';
+});
+
+
+// Manejar el evento popstate para volver a la página original al presionar el botón atrás del navegador
+window.addEventListener('popstate', (event) => {
+    // Revertir al contenido original si existe un estado en el historial
+    window.location.href = 'Prueba1.html';
+});
+
+const textoSVGM = document.createElementNS("http://www.w3.org/2000/svg", "text");
+let bbox = aulas01.getBBox();
+let x = bbox.x + bbox.width / 2 -1;
+let y = bbox.y + 53; 
+textoSVGM.setAttribute("x", x);
+textoSVGM.setAttribute("y", y); // Ajusta la posición vertical según tus necesidades
+textoSVGM.setAttribute("text-anchor", "middle");  
+textoSVGM.setAttribute('class', 'nombreMapa'); 
+textoSVGM.textContent = 'Aulas 1';
+svg.appendChild(textoSVGM);
+
+const textoSVGM2 = document.createElementNS("http://www.w3.org/2000/svg", "text");
+bbox = aulas02.getBBox();
+x = bbox.x + bbox.width / 2 -1;
+y = bbox.y + 53; 
+textoSVGM2.setAttribute("x", x);
+textoSVGM2.setAttribute("y", y-15); // Ajusta la posición vertical según tus necesidades
+textoSVGM2.setAttribute("text-anchor", "middle");  
+textoSVGM2.setAttribute('class', 'nombreMapa'); 
+textoSVGM2.textContent = 'Aulas 2';
+svg.appendChild(textoSVGM2);
+
+const textoSVGM3 = document.createElementNS("http://www.w3.org/2000/svg", "text");
+bbox = aulas03.getBBox();
+x = bbox.x + bbox.width / 2 -1;
+y = bbox.y + 53; 
+textoSVGM3.setAttribute("x", x);
+textoSVGM3.setAttribute("y", y-3); // Ajusta la posición vertical según tus necesidades
+textoSVGM3.setAttribute("text-anchor", "middle");  
+textoSVGM3.setAttribute('class', 'nombreMapa'); 
+textoSVGM3.textContent = 'Aulas 3';
+svg.appendChild(textoSVGM3);
+
+const textoSVGM4 = document.createElementNS("http://www.w3.org/2000/svg", "text");
+bbox = aulas04.getBBox();
+x = bbox.x + bbox.width / 2 -1;
+y = bbox.y + 53; 
+textoSVGM4.setAttribute("x", x);
+textoSVGM4.setAttribute("y", y-13); // Ajusta la posición vertical según tus necesidades
+textoSVGM4.setAttribute("text-anchor", "middle");  
+textoSVGM4.setAttribute('class', 'nombreMapa'); 
+textoSVGM4.textContent = 'Aulas 4';
+svg.appendChild(textoSVGM4);
+
+const textoSVGM5 = document.createElementNS("http://www.w3.org/2000/svg", "text");
+bbox = Laboratorios.getBBox();
+x = bbox.x + bbox.width / 2 -1;
+y = bbox.y + 53; 
+textoSVGM5.setAttribute("x", x+2);
+textoSVGM5.setAttribute("y", y-15); // Ajusta la posición vertical según tus necesidades
+textoSVGM5.setAttribute("text-anchor", "middle");  
+textoSVGM5.setAttribute('class', 'nombreMapa'); 
+textoSVGM5.textContent = 'Laboratorios';
+svg.appendChild(textoSVGM5);
+
+const textoSVGM5_1 = document.createElementNS("http://www.w3.org/2000/svg", "text");
+bbox = Laboratorios.getBBox();
+x = bbox.x + bbox.width / 2 -1;
+y = bbox.y + 53; 
+textoSVGM5_1.setAttribute("x", x+2);
+textoSVGM5_1.setAttribute("y", y+10); // Ajusta la posición vertical según tus necesidades
+textoSVGM5_1.setAttribute("text-anchor", "middle");  
+textoSVGM5_1.setAttribute('class', 'nombreMapa'); 
+textoSVGM5_1.textContent = 'Edificio central';
+svg.appendChild(textoSVGM5_1);
+
+const textoSVGM6 = document.createElementNS("http://www.w3.org/2000/svg", "text");
+const textoSVGM6_2 = document.createElementNS("http://www.w3.org/2000/svg", "text");
+const textoSVGM6_3 = document.createElementNS("http://www.w3.org/2000/svg", "text");
+bbox = Epesados.getBBox();
+x = bbox.x + bbox.width / 2 -1;
+y = bbox.y + 53; 
+textoSVGM6.setAttribute("x", x+3);
+textoSVGM6.setAttribute("y", y-15); // Ajusta la posición vertical según tus necesidades
+textoSVGM6.setAttribute("text-anchor", "middle");  
+textoSVGM6.setAttribute('class', 'nombreMapa'); 
+textoSVGM6.textContent = 'Edificio';
+svg.appendChild(textoSVGM6);
+
+textoSVGM6_2.setAttribute("x", x+3);
+textoSVGM6_2.setAttribute("y", y+5); // Ajusta la posición vertical según tus necesidades
+textoSVGM6_2.setAttribute("text-anchor", "middle");  
+textoSVGM6_2.setAttribute('class', 'nombreMapa'); 
+textoSVGM6_2.textContent = 'de';
+svg.appendChild(textoSVGM6_2);
+
+textoSVGM6_3.setAttribute("x", x+3);
+textoSVGM6_3.setAttribute("y", y+25); // Ajusta la posición vertical según tus necesidades
+textoSVGM6_3.setAttribute("text-anchor", "middle");  
+textoSVGM6_3.setAttribute('class', 'nombreMapa'); 
+textoSVGM6_3.textContent = 'pesados';
+svg.appendChild(textoSVGM6_3);
+
+
